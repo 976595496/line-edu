b3df263436feddb8d4330640cf9898d095eb4138
zhangcc <zhangcz@bjttsf.com>
2020-04-16 15:59:25 +0800

    mybatis-plus配置说明

--- java/line-edu/batis/src/main/java/com/zcz/study/batis/config/mybatis/MybatisConfig.java
+++ java/line-edu/batis/src/main/java/com/zcz/study/batis/config/mybatis/MybatisConfig.java
@@ -22,21 +22,45 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @MapperScan
 public class MybatisConfig {
 
+    /**
+     * @Author zcz
+     * @Description 乐观锁
+     * @Date 2020/4/16  15:57
+     * @Return
+     */
     @Bean
     public OptimisticLockerInterceptor optimisticLockerInterceptor(){
         return new OptimisticLockerInterceptor();
     }
-    
+
+    /**
+     * @Author zcz
+     * @Description 分页
+     * @Date 2020/4/16  15:58
+     * @Return
+     */
     @Bean
     public PaginationInterceptor paginationInterceptor(){
         return new PaginationInterceptor();
     }
 
+    /**
+     * @Author zcz
+     * @Description 逻辑删除
+     * @Date 2020/4/16  15:58
+     * @Return
+     */
     @Bean
     public ISqlInjector sqlInjector(){
         return new LogicSqlInjector();
     }
 
+    /**
+     * @Author zcz
+     * @Description 性能监控
+     * @Date 2020/4/16  15:58
+     * @Return
+     */
     @Bean
     @Profile({"dev","test"})
     public PerformanceInterceptor performanceInterceptor(){
