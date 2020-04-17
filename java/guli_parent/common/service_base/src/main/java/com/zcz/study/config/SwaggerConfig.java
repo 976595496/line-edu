be4a7cfc3afa4832d8a2ed675416eccee762e0e9
zhangcc <zhangcz@bjttsf.com>
2020-04-17 19:29:35 +0800

    swagger

--- java/guli_parent/common/service_base/src/main/java/com/zcz/study/config/SwaggerConfig.java
+++ java/guli_parent/common/service_base/src/main/java/com/zcz/study/config/SwaggerConfig.java
@@ -35,8 +35,8 @@ public class SwaggerConfig {
     private ApiInfo webApiInfo(){
 
         return new ApiInfoBuilder()
-                .title("网站-课程中心API文档")
-                .description("本文档描述了课程中心微服务接口定义")
+                .title("学习项目接口文档")
+                .description("文档描述")
                 .version("1.0")
                 .contact(new Contact("java", "http://zcz.com", "zcz@qq.com"))
                 .build();
